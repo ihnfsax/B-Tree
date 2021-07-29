3ff216c1b5bfab0a6b25671a5fff8ce00adb7cc4
hades <kylelynx7289@gmail.com>
2021-07-29 11:09:42 +0800

    Add insert function

--- cxx/BPlusTree.hpp
+++ cxx/BPlusTree.hpp
@@ -1,7 +1,9 @@
 #pragma once
+#include <deque>  // debug
 #include <functional>
 #include <iostream>
 #include <stdexcept>
+#include <string.h>
 
 namespace my {
 template <class T1, class T2> struct Pair {
@@ -13,7 +15,7 @@ template <class T1, class T2> struct Pair {
     Pair(const T1& t1, const T2& t2) : first(t1), second(t2){};
 };
 
-template <class Key, class T, class Compare = std::less<Key>> class BPlusTree {
+template <class Key, class T> class BPlusTree {
 public:
     typedef Key                key_type;
     typedef T                  data_type;
@@ -21,12 +23,35 @@ public:
     typedef short              order_type;
     typedef unsigned int       size_type;
 
-protected:
+    // protected:
     typedef unsigned int node_size_type;  // no need bigger than size_type
     typedef struct ListNode {
+        key_type*        key;
         data_type        data;
-        struct ListNode* prior;
-        struct ListNode* next;
+        struct ListNode* prior = nullptr;
+        struct ListNode* next  = nullptr;
+        ListNode();
+        ListNode(key_type* k) : key(k){};
+        ListNode(key_type* k, data_type d) : key(k), data(d){};
+        /* flag: true: insert before, false: insert after */
+        void insert(ListNode* p, bool flag) {
+            if (p == nullptr)
+                throw std::runtime_error("try to insert a null node");
+            if (flag) {
+                p->prior = this->prior;
+                p->next  = this;
+                if (this->prior)
+                    this->prior->next = p;
+                this->prior = p;
+                return;
+            }
+            p->prior = this;
+            p->next  = this->next;
+            if (this->next) {
+                this->next->prior = p;
+            }
+            this->next = p;
+        }
     } ListNode, *ListPtr;
 
     class BTIterator {
@@ -35,6 +60,13 @@ protected:
 
     public:
         BTIterator(ListPtr n) : node(n){};
+
+        const key_type& key() {
+            if (node == nullptr) {
+                throw std::runtime_error("try to get key from nullptr");
+            }
+            return node->key;
+        }
 
         data_type& data() {
             if (node == nullptr) {
@@ -74,40 +106,101 @@ protected:
     class BTNode {
     public:
         const bool type;  // 0: inner node; 1: leaf node
-        key_type*  key   = nullptr;
-        BTNode**   child = nullptr;
-        ListPtr*   entry = nullptr;
-        order_type count = 0;
-        BTNode(order_type m, bool type) : type(type) {
+        key_type*  key    = nullptr;
+        BTNode**   child  = nullptr;
+        ListPtr*   entry  = nullptr;
+        BTNode*    parent = nullptr;
+        order_type count  = 0;
+        order_type order;
+        BTNode(order_type m, bool type) : order(m), type(type) {
+            if (m < 3) {
+                throw std::runtime_error("order of BPlusTree must >= 3");
+            }
             key = new key_type[m];
-            if (type) {
+            if (type)
                 entry = new ListPtr[m];
-            }
-            else {
+            else
                 child = new BTNode*[m];
-            }
         }
         ~BTNode() {
             delete[] key;
-            if (type) {
+            if (type)
                 delete[] entry;
-            }
-            else {
+            else
                 delete[] child;
+        }
+
+        order_type search(const key_type& k) const {
+            if (count == 0)
+                return -2;
+            if (k < key[0])
+                return -1;
+            order_type i = 0;
+            for (; i < count - 1; ++i) {
+                if (k < key[i + 1])
+                    return i;
             }
+            return i;
+        }
+
+        order_type insert(const order_type& r, const key_type& k, const data_type* const d = nullptr) {
+            if (type && d == nullptr)
+                throw std::runtime_error("try to insert a nullptr to data field");
+            for (order_type i = count - 1; i > r; --i) {
+                key[i + 1] = key[i];
+                if (type) { /* leaf node */
+                    entry[i + 1]      = entry[i];
+                    entry[i + 1]->key = &key[i + 1];
+                }
+                else /* inner node */
+                    child[i + 1] = child[i];
+            }
+            count++;
+            key[r + 1] = k;
+            if (type) { /* leaf node */
+                entry[r + 1] = new ListNode(&key[r + 1], *d);
+                if (count != 1) {
+                    if (r + 1 == count - 1)
+                        entry[r]->insert(entry[r + 1], false); /* insert after */
+                    else
+                        entry[r + 2]->insert(entry[r + 1], true); /* insert before */
+                }
+            }
+            else /* inner node */
+                child[r + 1] = nullptr;
+            return r + 1;
+        }
+
+        BTNode* splitSelf(order_type s) {
+            if (s >= count - 1 || s < -1)
+                throw std::runtime_error("can't split node");
+            BTNode* node = new BTNode(order, type);
+            node->count  = count - s - 1;
+            memcpy(node->key, &key[s + 1], node->count * sizeof(key_type));
+            if (type)
+                memcpy(node->entry, &entry[s + 1], node->count * sizeof(ListPtr));
+            else {
+                memcpy(node->child, &child[s + 1], node->count * sizeof(BTNode*));
+                for (order_type i = 0; i < node->count; ++i) {
+                    node->child[i]->parent = node;
+                }
+            }
+            count        = s + 1;
+            node->parent = parent;
+            return node;
         }
     };
 
     size_type      _size       = 0;  // 数据数量
     node_size_type _node_count = 0;  // 节点数量
     order_type     _order;
-    BTNode*        _root;
-    ListPtr        _head;
+    BTNode*        _root = nullptr;
+    ListPtr        _head = nullptr;
 
 public:
     BPlusTree(order_type order = 3) : _order(order) {
-        if (order < 0) {
-            throw std::runtime_error("order of BPlusTree can not be a negative number");
+        if (order < 3) {
+            throw std::runtime_error("order of BPlusTree must >= 3");
         }
     };
 
@@ -119,6 +212,96 @@ public:
         return _order;
     }
 
-    BTIterator find(const key_type& k) {}
-};
+    BTIterator find(const key_type& key) {}
+
+    bool insert(const key_type& key, const data_type& data) {
+        BTNode* v = _root;
+        if (v == nullptr) {
+            /* only for _root */
+            v = new BTNode(_order, true);
+            v->insert(-1, key, &data);
+            _root = v;
+            return true;
+        }
+        while (true) {
+            order_type r = v->search(key);
+            if (v->type) {
+                if (r >= 0 && v->key[r] == key)
+                    return false;  // 插入失败
+                doInsert(v, r, key, data);
+                return true;
+            }
+            else {
+                if (r == -1)
+                    v->key[0] = key, r = 0;
+                v = v->child[r];
+            }
+        }
+    }
+    /* for debug */
+    void print() {
+        BTNode*             v = nullptr;
+        std::deque<BTNode*> pnodes, cnodes;
+        int                 l = 0;
+        if (_root != nullptr) {
+            pnodes.push_back(_root);
+            while (!pnodes.empty()) {
+                std::cout << "Level " << l++ << ": ";
+                while (!pnodes.empty()) {
+                    v = *(pnodes.begin());
+                    pnodes.pop_front();
+                    std::cout << "|";
+                    for (order_type i = 0; i < v->count; ++i) {
+                        if (v->type) {
+                            std::cout << v->key[i] << ":" << v->entry[i]->data << "|";
+                        }
+                        else {
+                            std::cout << v->key[i] << "|";
+                            cnodes.push_back(v->child[i]);
+                        }
+                    }
+                    if (!pnodes.empty()) {
+                        std::cout << "-----";
+                    }
+                }
+                std::cout << "\n";
+                pnodes = cnodes;
+                cnodes.clear();
+            }
+        }
+    }
+
+private:
+    void doInsert(BTNode* n, order_type& r, const key_type& k, const data_type& d) {
+        if (n->count < _order) {
+            r = n->insert(r, k, &d);
+        }
+        else {
+            order_type s  = _order / 2;
+            BTNode*    n2 = nullptr;
+            if (r < s) {
+                n2 = n->splitSelf(s - 1);
+                r  = n->insert(r, k, &d);
+            }
+            else {
+                n2 = n->splitSelf(s);
+                r  = n2->insert(r - s - 1, k, &d);
+            }
+            if (n->parent) {
+                order_type r2 = n->parent->search(n2->key[0]);
+                doInsert(n->parent, r2, n2->key[0], d);
+                n->parent->child[r2] = n2;
+            }
+            else {
+                n->parent = new BTNode(_order, false);
+                n->parent->insert(-1, n->key[0]);
+                n->parent->insert(0, n2->key[0]);
+                n->parent->child[0] = n;
+                n->parent->child[1] = n2;
+                n2->parent          = n->parent;
+                _root               = n->parent;
+            }
+        }
+    }
+};  // namespace my
 }  // namespace my
--- cxx/main.cpp
+++ cxx/main.cpp
@@ -1,13 +1,63 @@
 #include "BPlusTree.hpp"
 #include <iostream>
+#include <string>
 #include <vector>
 
-struct Node {
-    int a;
-};
-
 int main(int argc, char* argv[]) {
-    Node* ptr = nullptr;
-    std::cout << ptr->a;
+    // my::BPlusTree<int, std::string>::BTNode node(5, true);
+    // node.key[0]                                 = 1;
+    // node.key[1]                                 = 10;
+    // node.key[2]                                 = 100;
+    // my::BPlusTree<int, std::string>::ListPtr d1 = new my::BPlusTree<int, std::string>::ListNode(&node.key[0], "A");
+    // my::BPlusTree<int, std::string>::ListPtr d2 = new my::BPlusTree<int, std::string>::ListNode(&node.key[1], "B");
+    // my::BPlusTree<int, std::string>::ListPtr d3 = new my::BPlusTree<int, std::string>::ListNode(&node.key[2], "C");
+    // d1->next                                    = d2;
+    // d1->prior                                   = nullptr;
+    // d2->next                                    = d3;
+    // d2->prior                                   = d1;
+    // d3->prior                                   = d2;
+    // d3->next                                    = nullptr;
+    // node.entry[0]                               = d1;
+    // node.entry[1]                               = d2;
+    // node.entry[2]                               = d3;
+    // node.count                                  = 3;
+    // std::cout << node.search(1) << "\n";
+    // std::cout << node.search(8) << "\n";
+    // std::cout << node.search(80) << "\n";
+    // std::cout << node.search(100) << "\n";
+    // std::cout << node.search(102) << "\n";
+    // std::cout << node.search(-1) << "\n";
+    // std::cout << node.search(0) << "\n";
+    // node.insert(0, 5, "hello");
+    // node.insert(-1, -1, "ukpkmkk");
+    // node.insert(4, 130, "hhh");
+    // my::BPlusTree<int, std::string>::ListPtr p = node.entry[0];
+    // while (p) {
+    //     std::cout << *(p->key) << ": " << p->data << "\n";
+    //     p = p->next;
+    // }
+    my::BPlusTree<int, std::string> btree(5);
+    std::cout << btree.insert(1, "123") << "\n";
+    std::cout << btree.insert(3, "hell") << "\n";
+    std::cout << btree.insert(-1, "before") << "\n";
+    std::cout << btree.insert(2, "doom") << "\n";
+    std::cout << btree.insert(8, "poll") << "\n";
+    std::cout << btree.insert(9, "kkk") << "\n";
+    std::cout << btree.insert(9, "kkk") << "\n";
+    std::cout << btree.insert(10, "kkk") << "\n";
+    std::cout << btree.insert(11, "kkk") << "\n";
+    std::cout << btree.insert(13, "kkk") << "\n";
+    std::cout << btree.insert(4, "kkk") << "\n";
+    std::cout << btree.insert(6, "kkk") << "\n";
+    std::cout << btree.insert(-4, "kkk") << "\n";
+    std::cout << btree.insert(-7, "kkk") << "\n";
+    std::cout << btree.insert(-2, "kkk") << "\n";
+    std::cout << btree.insert(5, "kkk") << "\n";
+    std::cout << btree.insert(7, "kkk") << "\n";
+    std::cout << btree.insert(15, "kkk") << "\n";
+    std::cout << btree.insert(16, "kkk") << "\n";
+    std::cout << btree.insert(17, "kkk") << "\n";
+    // std::cout << btree.insert(18, "kkk") << "\n";
+    btree.print();
     return 0;
 }
